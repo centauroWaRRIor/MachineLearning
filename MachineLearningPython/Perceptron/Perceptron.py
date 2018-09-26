e80e1866d48152cf02b353effc8495347619d2da
Victor E. Villagomez <emmanuel.villagomez@gmail.com>
2018-09-25 23:49:58 -0700

    Able to read image on demand and visualize it

--- MachineLearningPython/Perceptron/Perceptron.py
+++ MachineLearningPython/Perceptron/Perceptron.py
@@ -3,41 +3,59 @@ import sys
 import struct
 import numpy as np
 
-def ascii_show(image):
-    for y in image:
-         row = ""
-         for x in y:
-             row += '{0: <4}'.format(x)
-         print row
+
+class Online_Datastream:
+	"""Simulates an infinite stream of examples"""
+
+	def __init__(self, training_filename):
+
+		# open file
+		self.training_file = open(training_filename, 'rb')
+
+		# extract magic number
+		self.training_file.seek(0)
+		self.magic_number = struct.unpack('>4B', self.training_file.read(4))
+
+		#32-bit integer (4 bytes)
+		self.training_file.seek(4)
+		self.num_images = struct.unpack('>I', self.training_file.read(4))[0]
+		print "num images: ", self.num_images
+		self.num_rows = struct.unpack('>I', self.training_file.read(4))[0]
+		print "num rows: ", self.num_rows
+		self.num_columns = struct.unpack('>I', self.training_file.read(4))[0]
+		print "num columns: ", self.num_columns
+
+		self.image_bytes = self.num_rows * self.num_columns
+
+
+	def __del__(self):
+
+		self.training_file.close()
+
+
+	def show_image(self, image_index):
+
+		file_offset = 16 + image_index * self.image_bytes
+		self.training_file.seek(file_offset)
+		image_numpy_array = np.asarray(struct.unpack('>'+'B' * self.image_bytes, self.training_file.read(self.image_bytes))).reshape(self.num_rows, self.num_columns)
+		image_python_array = image_numpy_array.tolist()
+		self.ascii_show(image_python_array)
+
+
+	def ascii_show(self, image):
+		for y in image:
+			row = ""
+			for x in y:
+				row += '{0: <4}'.format(x)
+			print row
 
 def main():
 
 	training_filename = "C:/Users/Emmauel/Desktop/CS_578/hw2/train-images-idx3-ubyte/train-images.idx3-ubyte"
-
-	# open file
-	training_file = open(training_filename, 'rb')
-
-	# extract magic number
-	training_file.seek(0)
-	magic_number = struct.unpack('>4B', training_file.read(4))
-
-	#32-bit integer (4 bytes)
-	training_file.seek(4)
-	num_images = struct.unpack('>I', training_file.read(4))[0]
-	print "num images: ", num_images
-	num_rows = struct.unpack('>I',training_file.read(4))[0]
-	print "num rows: ", num_rows
-	num_columns = struct.unpack('>I',training_file.read(4))[0]
-	print "num columns: ", num_columns
-
-	image_bytes = num_rows * num_columns
-
-	for i in range(3):
-		image_numpy_array = np.asarray(struct.unpack('>'+'B' * image_bytes, training_file.read(image_bytes))).reshape(num_rows, num_columns)
-		image_python_array = image_numpy_array.tolist()
-		ascii_show(image_python_array)
-
-	training_file.close()
+	#label_filename = "C:/Users/Emmauel/Desktop/CS_578/hw2/train-images-idx3-ubyte/train-images.idx3-ubyte"
+	data_stream = Online_Datastream(training_filename)
+	data_stream.show_image(1)
+	
 	return 0
 
 if __name__ == "__main__":

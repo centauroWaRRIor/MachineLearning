9fb79e5dfd006b3dbd999f779ca87b36b4cd5715
Victor E. Villagomez <emmanuel.villagomez@gmail.com>
2018-09-29 15:55:09 -0700

    Added a validation pass on the test set

--- MachineLearningPython/Perceptron/Perceptron.py
+++ MachineLearningPython/Perceptron/Perceptron.py
@@ -279,7 +279,7 @@ class Digit_Classifier_Vanilla:
 			largest_w_dot_x_index = 0 # by default predict 0
 			for i in range(10):
 				w_dot_x = self.perceptrons[i].predict_w_dot_x(inputs)
-				#print ("predicted w_dot_x: ", w_dot_x)
+				print ("predicted w_dot_x: ", w_dot_x)
 				if w_dot_x > largest_w_dot_x:
 					largest_w_dot_x = w_dot_x
 					largest_w_dot_x_index = i
@@ -295,15 +295,20 @@ def main():
 
 	images_filename = "C:/Users/Emmauel/Desktop/CS_578/hw2/train-images-idx3-ubyte/train-images.idx3-ubyte"
 	labels_filename = "C:/Users/Emmauel/Desktop/CS_578/hw2/train-labels-idx1-ubyte/train-labels.idx1-ubyte"
-	data_stream = MNIST_Datastream(images_filename, labels_filename)
-	#image_python_array = data_stream.get_image(33)
-	#data_stream.ascii_show(image_python_array)
-	#print data_stream.get_label(33)
+	training_data_stream = MNIST_Datastream(images_filename, labels_filename)
+
+	images_filename = "C:/Users/Emmauel/Desktop/CS_578/hw2/t10k-images-idx3-ubyte/t10k-images.idx3-ubyte"
+	labels_filename = "C:/Users/Emmauel/Desktop/CS_578/hw2/t10k-labels-idx1-ubyte/t10k-labels.idx1-ubyte"
+	test_data_stream = MNIST_Datastream(images_filename, labels_filename)
+
+	#image_python_array = training_data_stream.get_image(33)
+	#training_data_stream.ascii_show(image_python_array)
+	#print training_data_stream.get_label(33)
 	#print "--------------"
-	#image_python_array = data_stream.get_rounded_image(33)
-	#data_stream.ascii_show(image_python_array)
+	#image_python_array = training_data_stream.get_rounded_image(33)
+	#training_data_stream.ascii_show(image_python_array)
 	
-	#image_python_array = data_stream.get_rounded_1d_image(33)
+	#image_python_array = training_data_stream.get_rounded_1d_image(33)
 	#print image_python_array
 	
 	#f1_scoring = Classifier_Score()
@@ -317,16 +322,28 @@ def main():
 	ground_truth_labels = []
 
 	for i in range(500):
-		feature_inputs = data_stream.get_rounded_1d_image(i)
+		feature_inputs = training_data_stream.get_rounded_1d_image(i)
 		# augment the inputs with the bias term
 		feature_inputs.append(1)
 		inputs_vector.append(feature_inputs)
-		ground_truth_labels.append(data_stream.get_label(i))
+		ground_truth_labels.append(training_data_stream.get_label(i))
 
 	vanilla_classifier = Digit_Classifier_Vanilla()
 	vanilla_classifier.train(50, inputs_vector, ground_truth_labels, 0.001)
 
 	# Predict the training data
+	print "Predict the training data: "
+	vanilla_classifier.predict(inputs_vector, ground_truth_labels)
+
+	print "Predict the test data: "
+	inputs_vector = []
+	ground_truth_labels = []
+	for i in range(500):
+		feature_inputs = test_data_stream.get_rounded_1d_image(i)
+		# augment the inputs with the bias term
+		feature_inputs.append(1)
+		inputs_vector.append(feature_inputs)
+		ground_truth_labels.append(test_data_stream.get_label(i))
 	vanilla_classifier.predict(inputs_vector, ground_truth_labels)
 
 	return 0

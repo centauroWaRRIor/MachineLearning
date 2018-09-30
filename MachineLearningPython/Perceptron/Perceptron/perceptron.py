9586345aca30d3df457f359dcc949aa6b287fed1
Victor E. Villagomez <emmanuel.villagomez@gmail.com>
2018-09-30 16:55:35 -0700

    Added Winnow algo, average perceptron and added arg parsing. Haven't tested any of that code.

--- MachineLearningPython/Perceptron/Perceptron/perceptron.py
+++ MachineLearningPython/Perceptron/Perceptron/perceptron.py
@@ -1,5 +1,5 @@
 class Vanilla_Perceptron:
-	""" Vanilla implementation of a perceptron with 784 features + bias
+	""" Vanilla implementation of a perceptron with 784 features + bias.
 	The 784 inputs come directly from MNIST hand written images once the vector
 	has been flattened"""
 	
@@ -51,8 +51,82 @@ class Vanilla_Perceptron:
 			binary_label = 0
 			if self.predict_label == label:
 				binary_label = 1
-			y_hat = binary_label - prediction		
+			y_hat = binary_label - prediction # decide whether or not we need promotion or demotion	
 			error = abs(y_hat)	# get error from real classification 0 or 1 
 			
 			for i in range(self.num_features):
 				self.weights[i] = self.weights[i] + (error * l_rate * y_hat * inputs[i]) 
+
+class Average_Perceptron(Vanilla_Perceptron):
+	""" Implementation of a perceptron that keeps a running average of weights 
+	in order to approximate the effect of having multiple hypthesis voting 
+	with 784 features + bias. The 784 inputs come directly from MNIST hand written 
+	images once the vector has been flattened"""
+
+	def __init__(self, predict_label, init_random_weights=False):
+		Vanilla_Perceptron.__init__(ppredict_label, init_random_weights)
+		self.average_weights = []
+
+	def train_weights_one_epoch(self, inputs_vector, ground_truth_labels, l_rate=1.00):
+
+		assert(len(inputs_vector) == len(ground_truth_labels))
+
+		for inputs, label in zip(inputs_vector, ground_truth_labels):
+
+			prediction = self.predict(inputs) # get predicted classificaion, 0 or 1
+			# label is a digit from 0 to 9 so need to normalize
+			binary_label = 0
+			if self.predict_label == label:
+				binary_label = 1
+			y_hat = binary_label - prediction # decide whether or not we need promotion or demotion
+			error = abs(y_hat)	# get error from real classification 0 or 1 
+			
+			for i in range(self.num_features):
+				self.weights[i] = self.weights[i] + (error * l_rate * y_hat * inputs[i])
+				# keep running average to approximate multiple hypothesis voting
+				self.average_weights[i] = self.average_weights[i] + self.weights[i]
+
+	def predict_average_w_dot_x(self, inputs):
+
+		assert(len(inputs) == len(self.average_weights))
+		
+		w_dot_x = 0.0
+		for i in range(self.num_features):
+			w_dot_x += inputs[i] * self.average_weights[i] 
+
+		if w_dot_x >= 0.0:
+			return w_dot_x
+		else:
+			return 0.0
+
+
+class Vanilla_Winnow(Vanilla_Perceptron):
+	""" Implementation of a winnow algo eith 784 features + bias. 
+	The 784 inputs come directly from MNIST hand written 
+	images once the vector has been flattened"""
+
+	def __init__(self, predict_label, init_random_weights=False):
+		Vanilla_Perceptron.__init__(ppredict_label, init_random_weights)
+		self.average_weights = []
+
+	def train_weights_one_epoch(self, inputs_vector, ground_truth_labels, l_rate=1.00):
+
+		assert(len(inputs_vector) == len(ground_truth_labels))
+
+		for inputs, label in zip(inputs_vector, ground_truth_labels):
+
+			prediction = self.predict(inputs) # get predicted classificaion, 0 or 1
+			# label is a digit from 0 to 9 so need to normalize
+			binary_label = 0
+			if self.predict_label == label:
+				binary_label = 1
+			y_hat = binary_label - prediction # decide whether or not we need promotion or demotion
+			error = abs(y_hat)	# get error from real classification 0 or 1 
+			
+			if error != 0:
+				for i in range(self.num_features):
+					if y_hat > 0 and inputs[i] > 0: 
+						self.weights[i] = self.weights[i] * 2.0 # Promotion
+					elif inputs[i] > 0: 
+						self.weights[i] = self.weights[i] / 2.0 # Demotion
+						
--- MachineLearningPython/Perceptron/vanilla_perceptron.py
+++ MachineLearningPython/Perceptron/vanilla_perceptron.py
@@ -1,39 +1,28 @@
 import sys
-import struct
-import random
-import numpy as np
+import os
+import argparse
 
 def main():
 
-	images_filename = "C:/Users/Emmauel/Desktop/CS_578/hw2/train-images-idx3-ubyte/train-images.idx3-ubyte"
-	labels_filename = "C:/Users/Emmauel/Desktop/CS_578/hw2/train-labels-idx1-ubyte/train-labels.idx1-ubyte"
-	training_data_stream = MNIST_Datastream(images_filename, labels_filename)
-
-	images_filename = "C:/Users/Emmauel/Desktop/CS_578/hw2/t10k-images-idx3-ubyte/t10k-images.idx3-ubyte"
-	labels_filename = "C:/Users/Emmauel/Desktop/CS_578/hw2/t10k-labels-idx1-ubyte/t10k-labels.idx1-ubyte"
-	test_data_stream = MNIST_Datastream(images_filename, labels_filename)
-
-	#image_python_array = training_data_stream.get_image(33)
-	#training_data_stream.ascii_show(image_python_array)
-	#print training_data_stream.get_label(33)
-	#print "--------------"
-	#image_python_array = training_data_stream.get_rounded_image(33)
-	#training_data_stream.ascii_show(image_python_array)
+	parser = argparse.ArgumentParser()
+	parser.add_argument('training_set_size', required=True, help="Size of training set.")
+	parser.add_argument('number_epochs', required=True, help="Number of epochs.")
+	parser.add_argument('learning_rate', required=True, help="Learning rate.")
+	parser.add_argument('data_path', required=True, help="Path to data folder.")
+	args = parser.parse_args(argv)
 	
-	#image_python_array = training_data_stream.get_rounded_1d_image(33)
-	#print image_python_array
-	
-	#f1_scoring = Classifier_Score()
-	#y_true = [0, 1, 2, 0, 1, 2]
-	#y_pred = [0, 2, 1, 0, 0, 1]
-	#for true, pred in zip(y_true, y_pred):
-	#	f1_scoring.record_result(true, pred)
-	#print "F1-Score: ", f1_scoring.get_macro_F1_score()
+	images_filename_path = os.path.join(os.path.abspath(args.data_path), "train-images.idx3-ubyte")
+	labels_filename_path = os.path.join(os.path.abspath(args.data_path), "train-labels.idx1-ubyte")
+	training_data_stream = MNIST_Datastream(images_filename_path, labels_filename_path)
+
+	images_filename_path = os.path.join(os.path.abspath(args.data_path), "t10k-images.idx3-ubyte")
+	labels_filename_path = os.path.join(os.path.abspath(args.data_path), "t10k-labels.idx1-ubyte")
+	test_data_stream = MNIST_Datastream(images_filename_path, labels_filename_path)
 
 	inputs_vector = []
 	ground_truth_labels = []
 
-	for i in range(500):
+	for i in range(int(args.training_set_size)):
 		feature_inputs = training_data_stream.get_rounded_1d_image(i)
 		# augment the inputs with the bias term
 		feature_inputs.append(1)
@@ -41,7 +30,7 @@ def main():
 		ground_truth_labels.append(training_data_stream.get_label(i))
 
 	vanilla_classifier = Digit_Classifier_Vanilla()
-	vanilla_classifier.train(50, inputs_vector, ground_truth_labels, 0.001)
+	vanilla_classifier.train(int(args.number_epochs), inputs_vector, ground_truth_labels, float(args.learning_rate))
 
 	# Predict the training data
 	print "Predict the training data: "
@@ -61,6 +50,5 @@ def main():
 	return 0
 
 if __name__ == "__main__":
-	sys.setrecursionlimit(5000)
 	#sys.exit(int(main() or 0)) # use for when running without debugging
 	main() # use for when debugging within Visual Studio
